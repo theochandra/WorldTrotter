c4ca3f050a52db821720e12dcbbd328a1fc11004
theodorous beatayuvens <theochandra27@gmail.com>
2021-03-07 22:14:49 +0700

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by theodorous beatayuvens on 07/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
